c1d330cb3c4232274a3b51452931dcfcbb826371
Mbaka-cmd <official.mercymbaka@gmail.com>
2026-03-10 08:49:22 +0300

    Fix co-curricular page - dynamic DB activities with image support

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import CoCurricularActivity, GalleryAlbum, GalleryPhoto, KCSEResult, NewsEvent
 
-# Register your models here.
+@admin.register(CoCurricularActivity)
+class CoCurricularActivityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'is_published', 'order']
+    list_filter = ['category', 'is_published']
+    list_editable = ['is_published', 'order']
+    search_fields = ['name']
+
+@admin.register(GalleryAlbum)
+class GalleryAlbumAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'is_published', 'event_date']
+    list_filter = ['category', 'is_published']
+
+@admin.register(GalleryPhoto)
+class GalleryPhotoAdmin(admin.ModelAdmin):
+    list_display = ['album', 'caption', 'order']
+
+@admin.register(KCSEResult)
+class KCSEResultAdmin(admin.ModelAdmin):
+    list_display = ['year', 'mean_grade', 'mean_points', 'is_published']
+    list_filter = ['is_published']
+
+@admin.register(NewsEvent)
+class NewsEventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'post_type', 'is_published', 'created_at']
+    list_filter = ['post_type', 'is_published']
