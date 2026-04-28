41941709d9984896f85f67c60a1e6323cff6759b
MERCY_ENG <official.mercymbaka@gmail.com>
2026-04-28 15:56:39 +0300

    stable admin bootstrap fix

--- accounts/apps.py
+++ accounts/apps.py
@@ -2,5 +2,24 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "accounts"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
+
+    def ready(self):
+        # Import inside ready() to avoid AppRegistryNotReady errors
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError, ProgrammingError
+
+        try:
+            User = get_user_model()
+
+            # Create admin only if it does not exist
+            if not User.objects.filter(email="official.mercymbaka@gmail.com").exists():
+                User.objects.create_superuser(
+                    email="official.mercymbaka@gmail.com",
+                    password="49474918",  # change later in production
+                )
+
+        except (OperationalError, ProgrammingError):
+            # Prevent crash during migrations / first deploy
+            pass
